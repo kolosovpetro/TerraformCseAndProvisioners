14611c9a1bdfdced01dda37dc90381d58e1c3f45
kolosovpetro <kolosovp94@gmail.com>
2025-02-24 21:41:57 +0100

    linux provisioner example

---
+++ examples/linux-vm-provisioner/variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  type        = string
+  description = "Resource group location"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix for all resources"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags for all resources"
+  default = {
+    Environment  = "DEV"
+    Owner        = "Terraform"
+    Autoshutdown = "OFF"
+  }
+}
